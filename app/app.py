c3ef048d2ddb7e9a18a7f71470642782b018075c
theriley106 <christopherlambert106@gmail.com>
2019-10-26 22:15:43 -0700

    Added all files

--- app/app.py
+++ app/app.py
@@ -5,6 +5,13 @@ import time
 import json
 import textstat
 import requests
+import traceback
+from pdf2image import convert_from_path
+from wand.color import Color
+from wand.image import Image
+from PIL import ImageFont, ImageDraw, ImageEnhance
+import PIL
+import pdfParse
 
 app = Flask(__name__)
 
@@ -14,13 +21,18 @@ DB = json.load(open("documentDataDB.json"))
 
 RESPONSES = [None]
 
+def send_text_to_api():
+	print convert_pdf_to_png("static/tesla.pdf")
+	return
+
 @app.route("/update")
 def updated():
 	print("updated")
-	
+	with open('documentDataDB.json', 'w') as outfile:
+		json.dump(DB, outfile, indent=4)
 	headers = {
-    'Authorization': 'Bearer eyJ0eXAiOiJKV1QiLCJhbGciOiJSUzI1NiIsImtpZCI6IjY4MTg1ZmYxLTRlNTEtNGNlOS1hZjFjLTY4OTgxMjIwMzMxNyJ9.eyJUb2tlblR5cGUiOjUsIklzc3VlSW5zdGFudCI6MTU3MjEyOTYyNiwiZXhwIjoxNTcyMTU4NDI2LCJVc2VySWQiOiIyMDVhMWYxNS0yNTc0LTQxMWQtYjZlMC1hMDcyZTRlNTMyNTMiLCJzaXRlaWQiOjEsInNjcCI6WyJzaWduYXR1cmUiLCJjbGljay5tYW5hZ2UiLCJvcmdhbml6YXRpb25fcmVhZCIsImdyb3VwX3JlYWQiLCJwZXJtaXNzaW9uX3JlYWQiLCJ1c2VyX3JlYWQiLCJ1c2VyX3dyaXRlIiwiYWNjb3VudF9yZWFkIiwiZG9tYWluX3JlYWQiLCJpZGVudGl0eV9wcm92aWRlcl9yZWFkIiwiZHRyLnJvb21zLnJlYWQiLCJkdHIucm9vbXMud3JpdGUiLCJkdHIuZG9jdW1lbnRzLnJlYWQiLCJkdHIuZG9jdW1lbnRzLndyaXRlIiwiZHRyLnByb2ZpbGUucmVhZCIsImR0ci5wcm9maWxlLndyaXRlIiwiZHRyLmNvbXBhbnkucmVhZCIsImR0ci5jb21wYW55LndyaXRlIl0sImF1ZCI6ImYwZjI3ZjBlLTg1N2QtNGE3MS1hNGRhLTMyY2VjYWUzYTk3OCIsImlzcyI6Imh0dHBzOi8vYWNjb3VudC1kLmRvY3VzaWduLmNvbS8iLCJzdWIiOiIyMDVhMWYxNS0yNTc0LTQxMWQtYjZlMC1hMDcyZTRlNTMyNTMiLCJhdXRoX3RpbWUiOjE1NzIxMjkyMTIsInB3aWQiOiJmYzUwYmZlNS05YzUzLTQwMGUtOGYzMC02YjU3MDY1MDVkYjIifQ.r5CE9QneRUa3C8oAyRD2f3a_Ue8HJAyq6mKO8tx9eFzaoL5LfTIV-Dj10kpFbCQ_pxbYMWVOZ2lVDc-folOYtls3jMY6-he9d1DneK3ZAxH5rk57lHke4LZZCHZzbosPUIkSv7BVEdQG0kv9Ne2zG0Fc4r-FHciMaGMVdeakmVEbv966Dp9wmlF0Bz6BabYfFzWiFaH8Go0JQli0Su0NVW8HiX1Fy9MEweAwpGE0Aega0tk1yemo0qB1TYwfIUln0-2anBapZ40l3fj5qDfJiLUNRkPKHj_LRoFKV3VUc6_suY5kEWxkVMWf8zchSbXAa9FXB5yULewkYoMIzjiirg',
-    'Accept': 'application/json',
+	'Authorization': 'Bearer eyJ0eXAiOiJKV1QiLCJhbGciOiJSUzI1NiIsImtpZCI6IjY4MTg1ZmYxLTRlNTEtNGNlOS1hZjFjLTY4OTgxMjIwMzMxNyJ9.eyJUb2tlblR5cGUiOjUsIklzc3VlSW5zdGFudCI6MTU3MjEyOTYyNiwiZXhwIjoxNTcyMTU4NDI2LCJVc2VySWQiOiIyMDVhMWYxNS0yNTc0LTQxMWQtYjZlMC1hMDcyZTRlNTMyNTMiLCJzaXRlaWQiOjEsInNjcCI6WyJzaWduYXR1cmUiLCJjbGljay5tYW5hZ2UiLCJvcmdhbml6YXRpb25fcmVhZCIsImdyb3VwX3JlYWQiLCJwZXJtaXNzaW9uX3JlYWQiLCJ1c2VyX3JlYWQiLCJ1c2VyX3dyaXRlIiwiYWNjb3VudF9yZWFkIiwiZG9tYWluX3JlYWQiLCJpZGVudGl0eV9wcm92aWRlcl9yZWFkIiwiZHRyLnJvb21zLnJlYWQiLCJkdHIucm9vbXMud3JpdGUiLCJkdHIuZG9jdW1lbnRzLnJlYWQiLCJkdHIuZG9jdW1lbnRzLndyaXRlIiwiZHRyLnByb2ZpbGUucmVhZCIsImR0ci5wcm9maWxlLndyaXRlIiwiZHRyLmNvbXBhbnkucmVhZCIsImR0ci5jb21wYW55LndyaXRlIl0sImF1ZCI6ImYwZjI3ZjBlLTg1N2QtNGE3MS1hNGRhLTMyY2VjYWUzYTk3OCIsImlzcyI6Imh0dHBzOi8vYWNjb3VudC1kLmRvY3VzaWduLmNvbS8iLCJzdWIiOiIyMDVhMWYxNS0yNTc0LTQxMWQtYjZlMC1hMDcyZTRlNTMyNTMiLCJhdXRoX3RpbWUiOjE1NzIxMjkyMTIsInB3aWQiOiJmYzUwYmZlNS05YzUzLTQwMGUtOGYzMC02YjU3MDY1MDVkYjIifQ.r5CE9QneRUa3C8oAyRD2f3a_Ue8HJAyq6mKO8tx9eFzaoL5LfTIV-Dj10kpFbCQ_pxbYMWVOZ2lVDc-folOYtls3jMY6-he9d1DneK3ZAxH5rk57lHke4LZZCHZzbosPUIkSv7BVEdQG0kv9Ne2zG0Fc4r-FHciMaGMVdeakmVEbv966Dp9wmlF0Bz6BabYfFzWiFaH8Go0JQli0Su0NVW8HiX1Fy9MEweAwpGE0Aega0tk1yemo0qB1TYwfIUln0-2anBapZ40l3fj5qDfJiLUNRkPKHj_LRoFKV3VUc6_suY5kEWxkVMWf8zchSbXAa9FXB5yULewkYoMIzjiirg',
+	'Accept': 'application/json',
 	}
 
 	res = requests.get('https://demo.docusign.net/clickapi/v1/accounts/82472fcb-a7cd-4ca1-831f-c2dbc8b9d423/clickwraps/dd2f59a4-6acf-400a-82da-8bb2cf5c3533/users', headers=headers)
@@ -39,26 +51,69 @@ def echo_socket_docusign(ws):
 				ws.send(str(RESPONSES[-1]))
 				prev = RESPONSES[-1]
 		except Exception as exp:
+			print traceback.print_exc()
 			print("Exception in get updated from docusign" + str(exp))
 		time.sleep(.1)
+
+def convert_pdf_to_png(filenameVal):
+	pdf = Image(filename=filenameVal, resolution=200)
+
+	pages = len(pdf.sequence)
+
+	image = Image(
+		width=pdf.width,
+		height=pdf.height * pages
+	)
+
+	for i in xrange(pages):
+		image.composite(
+			pdf.sequence[i],
+			top=pdf.height * i,
+			left=0
+		)
+	image.background_color = Color("white")
+	image.alpha_channel = 'remove'
+	image.save(filename="out.png")
+	return remove_points_from_image(filenameVal)
+
+def remove_points_from_image(filenameVal):
+	source_img = PIL.Image.open("out.png").convert("RGBA")
+	width, height = source_img.size
+	draw = ImageDraw.Draw(source_img)
+	for val in DB[filenameVal.partition("/")[2]].keys():
+		y = float(height * float(val))
+		# print(y)
+		draw.rectangle(((0, y), (width, y+40)), fill="white")
+	# draw.text((20, 70), "something123", font=ImageFont.truetype("font_path123"))
+	source_img.save("newFile.png", "png")
+	return pdfParse.Get_text_from_image("newFile.png")
+
+def extract_unused_sentences(fileName):
+	pages = convert_from_path('pdf_file', 500)
+	for page in pages:
+		convert_pdf_to_png("static/tesla.pdf").save('out.png', 'png')
+	DB[fileName].values()
+	return 
 
 @sockets.route('/chrisDocusignEndpointDotExe')
 def echo_socket(ws):
 	while True:
-		try:
-			# print(message)\
+		if not ws.closed:
 			message = ws.receive()
-			position, fileName = message.split(",")
-			smallFilename = fileName.replace("/static/", "")
-			if smallFilename not in DB:
-				DB[smallFilename] = {}
-			if position not in DB[smallFilename]:
-				DB[smallFilename][position] = 0
-			DB[smallFilename][position] += 1
-			ws.send(str(datetime.datetime.now()))
-		except Exception as exp:
-			print("ERROR" + str(exp))
-		# print(DB)
+			try:
+				# print(message)\
+				position, fileName = message.split(",")
+				smallFilename = fileName.replace("/static/", "")
+				if smallFilename not in DB:
+					DB[smallFilename] = {}
+				if position not in DB[smallFilename]:
+					DB[smallFilename][position] = 0
+				DB[smallFilename][position] += 1
+				ws.send(str(datetime.datetime.now()))
+			except Exception as exp:
+				print("ERROR" + str(exp))
+			print message
+			# print(DB)
 		time.sleep(.1)
 
 @app.route("/pdfAnalyticsGenerate/<documentName>")
@@ -77,28 +132,34 @@ def getData(documentName):
 	points = []
 	width = float(request.args.get("width"))
 	height = float(request.args.get("height"))
-	print(width, height)
+	# raw_input(str(width) + str(height))
 	for key, val in DB.get(documentName, {}).iteritems():
-		
-		# print(int(val/5))
+		try:
+			# print(int(val/5))
 
-		val = float(val)
-		if height == 0:
-			height = 100
-		y = height * (float(key))
-		if y < 100:
-			y += 100
-		for i in range(int(val / 5)):
-			x = 40
-			while x < width-20:
-				points.append({"x": x, "y": y, "value": val/5})
-				x += 20
-			y += 10
-	return jsonify(points)
+			val = float(val)
+			if height == 0:
+				height = 100
+			y = height * (float(key))
+			if y < 100:
+				y += 100
+			for i in range(int(val / 5)):
+				x = 40
+				while x < width-20:
+					points.append({"x": int(x), "y": int(y), "value": int(val/5)})
+					x += 10
+				y += 10
+		except:
+			pass
+	return jsonify({"data": points})
 
 @app.route('/echo_test', methods=['GET'])
 def echo_test():
 	return render_template('display.html')
+
+@app.route('/admin', methods=['GET'])
+def admin():
+	return render_template('admin.html')
 
 @app.route('/index', methods=['GET'])
 def index():
@@ -110,5 +171,6 @@ def text_analytics():
 	# return render_template('index.html')
 
 if __name__ == '__main__':
+	# send_text_to_api()
 	app.run(debug=True)
 	# Start with gunicorn -k flask_sockets.worker app:app
--- app/pdfParse.py
+++ app/pdfParse.py
@@ -10,7 +10,7 @@ DB = json.load(open("ocrCache.json"))
 print(type(DB))
 
 def Get_text_from_image(pdf_path):
-    if pdf_path in DB:
+    if False and pdf_path in DB:
         return DB[pdf_path]
     pdf=wi(filename=pdf_path,resolution=300)
     pdfImg=pdf.convert('jpeg')
@@ -23,7 +23,7 @@ def Get_text_from_image(pdf_path):
         im=Image.open(io.BytesIO(imgBlob))
         text=pytesseract.image_to_string(im,lang='eng')
         extracted_text.append(text)
-    text = ([i.replace("\n","") for i in extracted_text])
+    text = "\n".join(extracted_text)
     DB[pdf_path] = text
     with open('ocrCache.json', 'w') as outfile:
         json.dump(DB, outfile, indent=4)
